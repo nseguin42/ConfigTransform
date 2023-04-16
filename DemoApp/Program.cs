450f4a003583a3c2661824195125697ab128c863
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-04-16 01:51:38 -0500

    DemoApp: remove some unused stuff from Program.cs

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,19 +1,10 @@
-﻿using System.Security.Cryptography;
-using ConfigTransform;
+﻿using ConfigTransform;
 using DemoApp;
 using DemoApp.Settings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
-// Load AES key and IV from file
-var key = File.ReadAllBytes("key.bin");
-var iv = File.ReadAllBytes("iv.bin");
-var aes = Aes.Create();
-aes.Key = key;
-aes.IV = iv;
-aes.Padding = PaddingMode.PKCS7;
 var transformers = new Dictionary<string, IConfigTransformer>
 {
     {"aes", DecryptingConfigTransformer.CreateAesTransformer("key.bin", "iv.bin")}
@@ -30,9 +21,4 @@ var serviceProvider = new ServiceCollection().AddSingleton<IConfiguration>(decry
     .BuildServiceProvider();
 
 var service = serviceProvider.GetRequiredService<ExampleService>();
-service.PrintSettings();
-
-var databaseSettings = serviceProvider.GetRequiredService<IOptionsMonitor<DatabaseSettings>>()
-    .CurrentValue;
-databaseSettings.Server = "newServer";
 service.PrintSettings();
